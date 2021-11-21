e103e53e890e36fb4b89d44487c406256c8ae4bf
zhenli1347 <2503248255@qq.com>
2021-11-21 20:38:07 +0800

    Update main.c

--- chapter03/main.c
+++ chapter03/main.c
@@ -1,21 +1,21 @@
  #include<stdio.h>
-   2 #include"arraylist.h"
-   3 int
-   4 main(int argc,char* argv[])
-   5 {
-   6     List listex;
-   7     int size;
-   8     printf("Please input size:");
-   9     scanf("%d",&size);
-  10     if(listinit(&listex,size)){
-  11         printf("list init succeed!\n");
-  12     }
-  13 
-  14     int i;
-  15     for(i=0;i<size;i++){
-  16         printf("%d ",listex.list[i]);
-  17     }
-  18     printf("\n");
-  19     return 0;
-  20 }
- ~             
+    #include"arraylist.h"
+    int
+    main(int argc,char* argv[])
+    {
+        List listex;
+        int size;
+        printf("Please input size:");
+        scanf("%d",&size);
+       if(listinit(&listex,size)){
+           printf("list init succeed!\n");
+       }
+   
+       int i;
+       for(i=0;i<size;i++){
+           printf("%d ",listex.list[i]);
+       }
+       printf("\n");
+       return 0;
+   }
+              
