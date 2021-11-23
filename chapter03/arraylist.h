3dc6ae4d152526e41d3a710182295e540cb224bb
zhenli1347 <2503248255@qq.com>
2021-11-23 19:59:48 +0800

    Update arraylist.h

--- chapter03/arraylist.h
+++ chapter03/arraylist.h
@@ -1,33 +1,33 @@
 //创建以数组为基础的线性表
-   2 
-   3 
-   4 
-   5 
-   6 
-   7 #include<stdio.h>
-   8 #include<stdlib.h>
-   9 #include<time.h>
-  10 #define TRUE 1
-  11 #define FALSE 0
-  12 #define MAXSIZE 20
-  13 #define CORRECT 1
-  14 #define ERROR 0
-  15 
-  16 
-  17 typedef int Status;
-  18 typedef int Elemtype;
-  19 struct List{
-  20     int list[MAXSIZE];
-  21     int length;
-  22 };
-  23 typedef struct List List;
-  24 typedef struct List* Listp;
-  25 typedef int Elemtype;
-  26 
-  27 
-  28 
-  29 int listinit(Listp listname,int size);
-  30 int listinsert(Listp listname,int position,Elemtype number);
-  31 int listdelete(Listp listname,int position);
-  32 int listdestory(Listp listname );
- ~                                      
+    
+    
+    
+    
+    
+    #include<stdio.h>
+    #include<stdlib.h>
+    #include<time.h>
+   #define TRUE 1
+   #define FALSE 0
+   #define MAXSIZE 20
+   #define CORRECT 1
+   #define ERROR 0
+   
+   
+   typedef int Status;
+   typedef int Elemtype;
+   struct List{
+       int list[MAXSIZE];
+       int length;
+   };
+   typedef struct List List;
+   typedef struct List* Listp;
+   typedef int Elemtype;
+   
+   
+   
+   int listinit(Listp listname,int size);
+   int listinsert(Listp listname,int position,Elemtype number);
+   int listdelete(Listp listname,int position);
+   int listdestory(Listp listname );
+                                       
